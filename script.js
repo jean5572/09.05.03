49160fa26514a54a395e0587b25657a144006120
Jean Wong <jeanwong@Jeans-MacBook-Pro.local>
2020-11-09 15:52:40 +0100

    trial js

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-$(document).ready(function () {
-    $('.flip').click(function () {
-        $('.cont-flip').toggleClass('flipped');
-        return false;
-    });
+//DOMLOADED - JEAN
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM loaded');
 });
+
+
+//KONTAKT FORMULAR - VIVI
+
+
+const link = "http://creativeworld.dk/kea02/tema09/09.05.03/wordpress/wp-json/wp/v2/pages";
+
+document.addEventListener("DOMContentLoaded", hentData);
+
+
+async function hentData() {
+    const respons = await fetch(link);
+    forside = await respons.json();
+    vis(forside);
+    console.log('DataHentet')
+}
+
+function vis(forside) {
+    console.log(forside)
+    document.querySelector(".frontpage-values-parent").innerHTML = forside.excerpt.rendered;
+    //                document.querySelector(".beskrivelse").innerHTML = forside.content.rendered;
+
+}
