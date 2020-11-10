93b685af36df5bee00b3a4fa5e2bd9fd876de95b
Jean Wong <jeanwong@desktop-bo0gtnf.kea.local>
2020-11-10 11:49:59 +0100

    hoejskoleophold info hentet

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 //DOMLOADED - JEAN
-window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM loaded');
-});
-
-const link = "http://creativeworld.dk/kea02/tema09/09.05.03/wordpress/wp-json/wp/v2/pages/40";
-
-document.addEventListener("DOMContentLoaded", hentData);
-
-
-async function hentData() {
-    const respons = await fetch(link);
-    forside = await respons.json();
-    vis(forside);
-    console.log('DataHentet')
-}
-
-function vis(forside) {
-    console.log(forside)
-    document.querySelector(".frontpage-values-parent").innerHTML = forside.content.rendered;
-    //                document.querySelector(".beskrivelse").innerHTML = forside.content.rendered;
-
-}
+//window.addEventListener('DOMContentLoaded', (event) => {
+//    console.log('DOM loaded');
+//});
+//
+//const link = "http://creativeworld.dk/kea02/tema09/09.05.03/wordpress/wp-json/wp/v2/pages/40";
+//
+//document.addEventListener("DOMContentLoaded", hentData);
+//
+//
+//async function hentData() {
+//    const respons = await fetch(link);
+//    forside = await respons.json();
+//    vis(forside);
+//    console.log('DataHentet')
+//}
+//
+//function vis(forside) {
+//    console.log(forside)
+//    document.querySelector(".frontpage-values-parent").innerHTML = forside.content.rendered;
+//    //                document.querySelector(".beskrivelse").innerHTML = forside.content.rendered;
+//
+//}
