71525eb8ddd947b12852e0f9f059e5a4891ef357
Jean Wong <jeanwong@desktop-bo0gtnf.kea.local>
2020-11-10 11:13:55 +0100

    Indhentdata virker

--- script.js
+++ script.js
@@ -3,11 +3,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM loaded');
 });
 
-
-//KONTAKT FORMULAR - VIVI
-
-
-const link = "http://creativeworld.dk/kea02/tema09/09.05.03/wordpress/wp-json/wp/v2/pages";
+const link = "http://creativeworld.dk/kea02/tema09/09.05.03/wordpress/wp-json/wp/v2/pages/40";
 
 document.addEventListener("DOMContentLoaded", hentData);
 
@@ -21,7 +17,7 @@ async function hentData() {
 
 function vis(forside) {
     console.log(forside)
-    document.querySelector(".frontpage-values-parent").innerHTML = forside.excerpt.rendered;
+    document.querySelector(".frontpage-values-parent").innerHTML = forside.content.rendered;
     //                document.querySelector(".beskrivelse").innerHTML = forside.content.rendered;
 
 }
